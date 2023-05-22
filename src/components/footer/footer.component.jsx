066966807e36d4590b42bd09f6b7390eaddb86b2
Irgen Wiig Sørensen <Irgen_w.s@hotmail.com>
2023-05-22 10:16:25 +0200

    Small design fixes

--- src/components/footer/footer.component.jsx
+++ src/components/footer/footer.component.jsx
@@ -11,25 +11,25 @@ const Footer = () => {
   return (
     <footer>
       <div  className='footer-section'>
-      <p className='footer-copyright'> © Irgen Wiig Sørensen. All rights reserved</p>
-          <ul className='footer-follow-icons'>
-            {socialprofils.github && (
-              <li>
-                <a href={socialprofils.github}>
-                  <FaGithub />
-                </a>
-              </li>
-            )}
-            {socialprofils.linkedin && (
-              <li>
-                <a href={socialprofils.linkedin}>
-                  <FaLinkedin />
-                </a>
-              </li>
-            )}
-         </ul>
+        <h1 className='footer-logo-name'>IWS Design</h1>
+        <ul className='footer-follow-icons'>
+          {socialprofils.github && (
+            <li>
+              <a href={socialprofils.github}>
+                <FaGithub />
+              </a>
+            </li>
+          )}
+          {socialprofils.linkedin && (
+            <li>
+              <a href={socialprofils.linkedin}>
+                <FaLinkedin />
+              </a>
+            </li>
+          )}
+        </ul>
       
-          <h1 className='footer-logo-name'>IWS Design</h1>
+        <p className='footer-copyright'> © Irgen Wiig Sørensen. All rights reserved.</p>
       </div>
     </footer>
   )
