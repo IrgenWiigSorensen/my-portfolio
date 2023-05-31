410d8e75e0e4c14deefdb77fd27360ac3a776def
Irgen Wiig Sørensen <Irgen_w.s@hotmail.com>
2023-05-31 12:39:27 +0200

    Changes alignment and font-size on button text

--- src/components/projects/projects.component.jsx
+++ src/components/projects/projects.component.jsx
@@ -1,72 +1,60 @@
-import './projects.styles.scss';
+import "./projects.styles.scss";
 
-import {
-  FaGithub,
-} from "react-icons/fa";
+import { FaGithub } from "react-icons/fa";
 
-import { projects } from '../../content.options';
-import Modal from '../modal/modal.component';
+import { projects } from "../../content.options";
+import Modal from "../modal/modal.component";
 
 const Projects = () => {
-
-
-
-  return(
-    <section id='projects'>
+  return (
+    <section id="projects">
       <div className="projects-section">
-        <div  className='section-header'>
+        <div className="section-header">
           <h1>Projects</h1>
         </div>
         <div className="projects-container">
+          {projects.map((data, i) => {
+            const { project, info, img, alt, website, joke, github } = data;
 
-            {projects.map((data, i) => {
-              const { project, info, img, alt, website, joke, github } = data; 
-
-                return (
-                  <div className='project-box' key={i}>
-                    <div className="project-img-container">
-                      <img src={img} alt={alt}/>
-                    </div>
-                  
-                  <div className='project-bottom-container'>
-                    <div className="project-text-container">
-                      <div>
-                        <h1>{project}</h1>
-                      </div>
-                      <div>
-                        <p>{info}</p>
-                      </div>
-                    </div>
-
-                    <div className="button-container">
-                        {website && (
-                            <button className='project-button'>
-                              <a href={website}>
-                                View
-                              </a>
-                            </button>
-                        )}
-                        {joke && (
-                              <Modal></Modal>
-                        )}
-                        {github && (
-                            <button className='project-button'>
-                              <a href={github}>
-                                <FaGithub />
-                              </a>
-                            </button>
-                        )}
-                    </div> 
-                  </div>
+            return (
+              <div className="project-box" key={i}>
+                <div className="project-img-container">
+                  <img src={img} alt={alt} />
                 </div>
 
-                );
-            })}
-            
+                <div className="project-bottom-container">
+                  <div className="project-text-container">
+                    <div>
+                      <h1>{project}</h1>
+                    </div>
+                    <div>
+                      <p>{info}</p>
+                    </div>
+                  </div>
+
+                  <div className="button-container">
+                    {website && (
+                      <button className="project-button">
+                        <a href={website}>View</a>
+                      </button>
+                    )}
+                    {joke && <Modal></Modal>}
+                    {github && (
+                      <button className="project-button">
+                        <a href={github}>
+                          <FaGithub />
+                        </a>
+                      </button>
+                    )}
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Projects; 
+export default Projects;
